2937a9ced31576667406c9814fd5645b51c2c289
adrianka1591 <adka1591@gmail.com>
2019-06-04 17:09:53 +0200

    Implement Api with Axios into Vuex

---
+++ client/src/components/Pagination.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="Pagination">
+       <v-app id="inspire">
+    <v-container fluid grid-list-md>
+      <v-data-iterator
+        :items="items"
+        :rows-per-page-items="rowsPerPageItems"
+        :pagination.sync="pagination"
+        content-tag="v-layout"
+        row
+        wrap
+      >
+        <template v-slot:item="props">
+          <v-flex
+            xs12
+            sm6
+            md4
+            lg3
+          >
+            <v-card>
+              <v-card-title><h4>{{ props.item.name }}</h4></v-card-title>
+              <v-divider></v-divider>
+              <v-list dense>
+                <v-list-tile>
+                  <v-list-tile-content>Calories:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Fat:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Carbs:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Protein:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.protein }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Sodium:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.sodium }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Calcium:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.calcium }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Iron:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.iron }}</v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </template>
+      </v-data-iterator>
+    </v-container>
+  </v-app>
+</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      name: 'Pagination',
+      rowsPerPageItems: [4, 8, 12],
+    pagination: {
+      rowsPerPage: 4
+    },
+    items: [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        sodium: 87,
+        calcium: '14%',
+        iron: '1%'
+      }]
+    }
+  }
+}
+
+      </script>
---
+++ client/src/views/Shop.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col-md-3" v-for="product in products" :key="product.id" >
+         <div class="card" data-toggle="modal" data-target="#productModal">
+           <img :src="product.images.primary.large" :alt="product.brand.name" class="card-img-top">
+           <div class="card-body">
+             <h4 class="card-title">{{ product.brand.name }}</h4>
+             <div class="card-text">{{ product.id }}</div>
+              <div class="tools">
+               <input type="number" placeholder="quantity"/>
+               <button class="btn btn-primary"><i class="material-icons">add_shopping_cart</i></button>
+              </div>
+           </div>
+         </div>
+      </div>
+    </div>
+    <Pagination/>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Pagination from '@/components/Pagination.vue'
+export default {
+  name: 'Shop',
+  components: {
+    Pagination
+  },
+  mounted () {
+      this.$store.dispatch('loadProducts')
+    },
+  computed: mapState(['products'])
+}
+</script>
+
+<style>
+.tools {
+  display: block;
+  flex-direction: row;
+  justify-content: center;
+  white-space: nowrap;
+  align-items: baseline;
+  margin-top: 10px;
+}
+input {
+  width: 34%;
+  height: 38px;
+  border-radius: 5px;
+  padding-bottom: 3px;
+  padding-left: 5px;
+
+}
+.btn {
+  width: 30%;
+  margin-left: 5px;
+}
+
+.material-icons {
+  padding-top: 2px;
+}
+
+</style>
